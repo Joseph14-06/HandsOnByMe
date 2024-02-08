90ae168503412e4896e5e668b94682a46b5f0d47
Joseph14-06 <nidhinmanoj1406@gmail.com>
2024-02-08 09:53:45 +0530

    7365

--- studtemp/src/App.js
+++ studtemp/src/App.js
@@ -1,7 +1,7 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 import StudentForm from './StudentForm';
-import "bootstrap/dist/css/bootstrap.min.css";
+// import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
